0dee9147f50fe77dec1395d326749f955ba8f09b
wen.li <wen.li@woqutech.com>
2021-12-01 10:47:25 +0800

    feat: 登录后跳转到发布管理页面

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -66,7 +66,7 @@ export default {
         if (valid) {
           loginAPI(this.loginData).then((res) => {
             if (res.description === 'success') {
-              this.$router.push('/home')
+              this.$router.push('/home/publish/manage')
               sessionStorage.setItem('userInfo', JSON.stringify(res.data[0]))
             } else {
               Message.error({ message: `${res.description}！` })
